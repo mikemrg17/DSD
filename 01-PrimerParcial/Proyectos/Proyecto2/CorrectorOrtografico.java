20e8acddac81705bb50cf9c676a2a2b85d273114
mikemrg17 <miguelangelmurga1234@gmail.com>
2022-11-05 18:50:00 -0600

    Optimization completed

--- 01-PrimerParcial/Proyectos/Proyecto2/CorrectorOrtografico.java
+++ 01-PrimerParcial/Proyectos/Proyecto2/CorrectorOrtografico.java
@@ -1,15 +1,20 @@
+/*
+	Proyecto 2: Primera parte
+	Alumno: Murga Dionicio Miguel Angel
+	Grupo: 4CM14
+*/
+
 import java.io.File;
-import java.util.ArrayList;
 import java.io.IOException;
 import java.io.FileNotFoundException;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.util.Scanner;
 import java.util.StringTokenizer;
+import java.util.HashSet;
 
 public class CorrectorOrtografico {
-
-	public static ArrayList<String> diccionario = new ArrayList<String>();
+	public static HashSet<String> diccionario = new HashSet<String>();
 
 	public static void main(String args[]) {
 		File carpeta = new File("./LIBROS_TXT");
@@ -19,43 +24,44 @@ public class CorrectorOrtografico {
 			analizarLibro(libro);
 		}
 
-		System.out.println("Diccionario:");
-		for(String palabra: diccionario)
-			System.out.println(palabra);
-
 		Scanner input = new Scanner(System.in);
 		String texto_usuario;
 		String[] palabras_usuario;
+		String opt = "";
 
-		System.out.println("\nInserte una cadena a evaluar");
-		texto_usuario = input.nextLine();
-		palabras_usuario = texto_usuario.split("\\s");
+		do {
+			System.out.println("Inserte una cadena a evaluar");
+			texto_usuario = input.nextLine();
+			palabras_usuario = texto_usuario.split("\\s");
 
-		System.out.println("Palabras erróneas: ");
-		for(int i = 0; i < palabras_usuario.length; i++)
-			if(!diccionario.contains(palabras_usuario[i].toLowerCase()))
-				System.out.println(palabras_usuario[i]);
+			System.out.println("Palabras erróneas: ");
+			for(int i = 0; i < palabras_usuario.length; i++)
+				if(!diccionario.contains(palabras_usuario[i]))
+					System.out.println(palabras_usuario[i]);
+
+			System.out.println("Otra cadena? Si: S | s");
+			opt = input.nextLine();
+		} while(opt.equals("S") || opt.equals("s")); 
+
+		
 	}
 
 	static void analizarLibro(File libro) {
 		try {
-			BufferedReader lector = new BufferedReader(new FileReader("LIBROS_TXT/" + libro.getName()));
-			System.out.println("Leyendo libro...");
+			BufferedReader lector = new BufferedReader(new FileReader("./LIBROS_TXT/" + libro.getName()));
       		String libro_texto = "";
       		String palabra;
+      		StringTokenizer st;
 
       		while ((libro_texto = lector.readLine()) != null) {
-      			StringTokenizer st = new StringTokenizer(libro_texto, ", ");
+      			st = new StringTokenizer(libro_texto, " ");
 	      		while(st.hasMoreTokens()){
 	      			palabra = st.nextToken();
 					if(!diccionario.contains(palabra)){
-						diccionario.add(palabra.toLowerCase());
+						diccionario.add(palabra);
 					}
 				}
       		}
-
-      		System.out.println("Libro leido");
-      		System.out.println("Palabras capturadas");
 		} catch(FileNotFoundException e) {
 			e.printStackTrace();
 		} catch(IOException e) {
